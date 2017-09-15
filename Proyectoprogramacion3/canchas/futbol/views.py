cb89fe36fe164fa51680b0364887d27f2e1e8183
brunoramosmarino <brunoramosmarino@gmail.com>
2017-09-15 08:29:00 -0300

    parte del login

--- Proyectoprogramacion3/canchas/futbol/views.py
+++ Proyectoprogramacion3/canchas/futbol/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.contrib.auth.models import User
 # Create your views here.
 def Inicio(request):
     return render(request , 'inicio.html')
 def Login(request):
     return render (request, 'login.html')
+def Createuser (request):
+    mail=""
+    name=""
+    password=""
+    user = User.objects.create_user(name, mail, password)
+    user.save()
