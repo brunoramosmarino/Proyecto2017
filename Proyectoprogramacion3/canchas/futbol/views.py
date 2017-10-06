720a95e6a9ded3be0940631a33b34f48a9b52111
diegogimenez04 <d.gimenez0101@gmail.com>
2017-10-06 08:44:58 -0300

    crearusuarios

--- Proyectoprogramacion3/canchas/futbol/views.py
+++ Proyectoprogramacion3/canchas/futbol/views.py
@@ -1,17 +1,37 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, render, redirect
+from django.template import RequestContext
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from .models import *
+
 # Create your views here.
+
 def Inicio(request):
     return render(request , 'inicio.html')
+
 def Createuser(request):
     return render (request, 'createuser.html')
+
 def Createusers (request):
     name=request.POST["usuario"]
-    password=request.POST["contraseña"]
+    password=request.POST["contrasenia"]
     mail=request.POST["email"]
     print name + password
-    user = User.objects.create_user(username="bruno", email="bruno@gmail.com", password="hola")
+    user = User.objects.create_user(name, password, mail)
     user.save()
-    return (request, 'createuser.html')
+    return render (request, 'createuser.html')
